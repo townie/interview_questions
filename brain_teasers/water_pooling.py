a3ee925f879f0b17ee6a4e395e88661dc0821736
Keith Webber <keith.e.webber@gmail.com>
2017-03-30 00:45:24 -0400

    added a few more categories

---
+++ brain_teasers/water_pooling.py
@@ -0,0 +1,5 @@
+# Find how much water would accumlate in a landscape described with 
+
+landscape = [1,1,3,2,1,5]
+
+volume = 3
---
+++ cs/order_sublist_by_list.py
@@ -0,0 +1,7 @@
+# Order sublist by order of parentlist 
+
+parentlist = [1,2,3,4,5,6]
+
+sublist = [5,4,6]
+
+output = [4,5,6]
